62e40c8deb793044c5efcc9dec60898f730753f4
Zachariah Chow <zachariahchow@gmail.com>
2020-08-25 00:23:14 +0800

    feat: Add >99 vote story bs function

---
+++ scrape.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+
+res = requests.get('https://news.ycombinator.com/news')
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+# Return stories that have more than 99 votes
+
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace('points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+
+
+pprint(create_custom_hn(links, subtext)[0])
